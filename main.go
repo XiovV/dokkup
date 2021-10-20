695ab104db5edaf8ba8be6f35594792ef1859545
XiovV <xiovv@tutanota.com>
2021-10-20 17:13:45 +0200

    slight refactor

--- main.go
+++ main.go
@@ -8,31 +8,36 @@ import (
 )
 
 func main() {
+	var node, container, apiKey string
+
 	actionCmd := flag.NewFlagSet("up", flag.ExitOnError)
-	node := actionCmd.String("node", "", "Node endpoint")
-	container := actionCmd.String("container", "", "Name of the container you wish to update")
-	image := actionCmd.String("image", "", "The image you'd like the container to be updated to")
-	apiKey := actionCmd.String("api-key", "", "Docker Control Agent API Key")
+	actionCmd.StringVar(&node, "node", "", "Node endpoint")
+	actionCmd.StringVar(&container, "container", "", "Name of the container you wish to update")
+	actionCmd.StringVar(&apiKey,"api-key", "", "Docker Control Agent API Key")
 	keep := actionCmd.Bool("keep", false, "Keep the previous version of the container. Useful if you ever need to use the 'rollback' command")
+	image := actionCmd.String("image", "", "The image you'd like the container to be updated to")
 
 	rollbackCmd := flag.NewFlagSet("rollback", flag.ExitOnError)
-	rollbackNode := rollbackCmd.String("node", "", "Node endpoint")
-	rollbackContainer := rollbackCmd.String("container", "", "Name of the container you wish to update")
-	rollbackApiKey := rollbackCmd.String("api-key", "", "Docker Control Agent API Key")
+	rollbackCmd.StringVar(&node, "node", "", "Node endpoint")
+	rollbackCmd.StringVar(&container, "container", "", "Name of the container you wish to update")
+	rollbackCmd.StringVar(&apiKey,"api-key", "", "Docker Control Agent API Key")
 
-	// TODO: clean this up
 	switch os.Args[1] {
 	case "up":
 		actionCmd.Parse(os.Args[2:])
-		dockerController := controller.NewDockerController(*node, *apiKey)
-		app := app.NewUpdate(*node, *container, *image, *keep, dockerController)
-		app.Run()
 	case "rollback":
 		rollbackCmd.Parse(os.Args[2:])
-		dockerController := controller.NewDockerController(*rollbackNode, *rollbackApiKey)
-		app := app.NewRollback(*rollbackNode, *rollbackContainer, dockerController)
-		app.Run()
 	}
 
+	dockerController := controller.NewDockerController(node, apiKey)
+
+	switch os.Args[1] {
+	case "up":
+		app := app.NewUpdate(node, container, *image, *keep, dockerController)
+		app.Run()
+	case "rollback":
+		app := app.NewRollback(node, container, dockerController)
+		app.Run()
+	}
 
 }
