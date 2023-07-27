fb53ba13476684e100e4e746eae021a5c39b9aa5
XiovV <xiovv@tutanota.com>
2023-07-27 12:20:19 +0200

    reading agent config

---
+++ config/agent_config.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type AgentConfig struct {
+  Port string `env:"PORT" env-default:"8080"`
+  APIKey string
+}
+
+func ReadAgentConfig() (*AgentConfig, error) {
+  var config AgentConfig
+
+  err := cleanenv.ReadEnv(&config)
+  if err != nil {
+    return nil, err
+  }
+
+  return &config, nil
+}
