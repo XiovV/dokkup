1e9346178d605846caa84ec9c99ffd92f44717b1
XiovV <xiovv@tutanota.com>
2023-08-14 13:24:57 +0200

    refactored the DeployJob RPC

--- pkg/docker/containers.go
+++ pkg/docker/containers.go
@@ -1,10 +1,15 @@
 package docker
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+
+	pb "github.com/XiovV/dokkup/pkg/grpc"
 )
 
 func (c *Controller) ContainerCreate(containerName string, containerConfig *container.Config, hostConfig *container.HostConfig) (container.CreateResponse, error) {
@@ -27,4 +32,58 @@ func (c *Controller) ContainerInspect(containerId string) types.ContainerJSON {
 	}
 
 	return resp
+}
+
+func (c *Controller) ContainerSetupConfig(config *pb.Container) *ContainerConfig {
+	uuid := uuid.New()
+	containerName := fmt.Sprintf("%s-%s", config.Name, uuid.String())
+
+	containerConfig := &container.Config{
+		Image:        config.Image,
+		ExposedPorts: nat.PortSet{},
+	}
+
+	hostConfig := &container.HostConfig{
+		PortBindings: nat.PortMap{},
+	}
+
+	for _, port := range config.Ports {
+		internalPort := fmt.Sprintf("%s/tcp", port.In)
+		containerConfig.ExposedPorts[nat.Port(internalPort)] = struct{}{}
+
+		hostConfig.PortBindings[nat.Port(internalPort)] = []nat.PortBinding{{HostIP: "0.0.0.0"}}
+	}
+
+	return &ContainerConfig{
+		ContainerName: containerName,
+		Config:        containerConfig,
+		HostConfig:    hostConfig,
+	}
+}
+
+func (c *Controller) CreateContainersFromRequest(request *pb.DeployJobRequest) ([]string, error) {
+	createdContainers := []string{}
+	for i := 0; i < int(request.Count); i++ {
+		containerConfig := c.ContainerSetupConfig(request.Container)
+
+		resp, err := c.ContainerCreate(containerConfig.ContainerName, containerConfig.Config, containerConfig.HostConfig)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		createdContainers = append(createdContainers, resp.ID)
+	}
+
+	return createdContainers, nil
+}
+
+func (c *Controller) StartContainers(containerIDs []string) error {
+	for _, container := range containerIDs {
+		if err := c.ContainerStart(container); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- pkg/server/containers.go
+++ pkg/server/containers.go
@@ -5,65 +5,29 @@ import (
 	"fmt"
 
 	pb "github.com/XiovV/dokkup/pkg/grpc"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/go-connections/nat"
-	"github.com/google/uuid"
 )
 
 func (s *Server) DeployJob(request *pb.DeployJobRequest, stream pb.Dokkup_DeployJobServer) error {
 	stream.Send(&pb.DeployJobResponse{Message: fmt.Sprintf("Attempting to pull image: %s", request.Container.Image)})
+
 	err := s.Controller.ImagePull(request.Container.Image)
 	if err != nil {
 		fmt.Println(err)
 		return errors.New("failed to pull image")
 	}
 
-
-  createdContainers := []string{}
-	for i := 0; i < int(request.Count); i++ {
-		uuid := uuid.New()
-		containerName := fmt.Sprintf("%s-%s", request.Container.Name, uuid.String())
-    
-    stream.Send(&pb.DeployJobResponse{Message: fmt.Sprintf("Setting up container: %s", containerName)})
-
-	  containerConfig := &container.Config{
-			Image:        request.Container.Image,
-			ExposedPorts: nat.PortSet{},
-		}
-
-		hostConfig := &container.HostConfig{
-			PortBindings: nat.PortMap{},
-		}
-
-		for _, port := range request.Container.Ports {
-			internalPort := fmt.Sprintf("%s/tcp", port.In)
-			containerConfig.ExposedPorts[nat.Port(internalPort)] = struct{}{}
-
-			hostConfig.PortBindings[nat.Port(internalPort)] = []nat.PortBinding{{HostIP: "0.0.0.0"}}
-		}
-
-    resp, err := s.Controller.ContainerCreate(containerName, containerConfig, hostConfig)
-		if err != nil {
-			fmt.Println(err)
-			return err
-		}
-
-    createdContainers = append(createdContainers, resp.ID)
-    
-    stream.Send(&pb.DeployJobResponse{Message: fmt.Sprintf("Container %s configured successfully", containerName)})
+	createdContainers, err := s.Controller.CreateContainersFromRequest(request)
+	if err != nil {
+		fmt.Println(err)
+		return err
 	}
 
-  for _, container := range createdContainers {
-    stream.Send(&pb.DeployJobResponse{Message: fmt.Sprintf("Attempting to start container: %s", container)})
+	err = s.Controller.StartContainers(createdContainers)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 
-    if err := s.Controller.ContainerStart(container); err != nil {
-      return err
-    }
-
-    stream.Send(&pb.DeployJobResponse{Message: "Container started successfully"})
-  }
-
-
-  stream.Send(&pb.DeployJobResponse{Message: "Deployment successfull"})
+	stream.Send(&pb.DeployJobResponse{Message: "Deployment successfull"})
 	return nil
 }
