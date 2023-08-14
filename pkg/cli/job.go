73204e2feada1c905f36b1b0c4a31e0068023e70
XiovV <xiovv@tutanota.com>
2023-08-14 16:09:53 +0200

    now calling the DeployJob RPC from the cli. The 'run job' command now works

--- pkg/cli/job.go
+++ pkg/cli/job.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"text/tabwriter"
@@ -56,7 +57,86 @@ func (a *App) jobCmd(ctx *cli.Context) error {
 		return nil
 	}
 
+	if err := a.deployJobs(inventory, job); err != nil {
+		log.Fatal(err)
+	}
+
 	return nil
+}
+
+func (a *App) deployJobs(inventory *config.Inventory, job *config.Job) error {
+	group, ok := inventory.GetGroup(job.Group)
+	if !ok {
+		return fmt.Errorf("couldn't find group: %s", group.Name)
+	}
+
+	for _, nodeName := range group.Nodes {
+		node, ok := inventory.GetNode(nodeName)
+		if !ok {
+			return fmt.Errorf("couldn't find node: %s", nodeName)
+		}
+
+		err := a.deployJob(node, job)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) deployJob(node config.Node, job *config.Job) error {
+	client, err := a.initClient(node.Location)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ctx = metadata.AppendToOutgoingContext(ctx, "authorization", node.Key)
+
+	request := a.jobToDeployJobRequest(job)
+
+	stream, err := client.DeployJob(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(resp)
+	}
+
+	return nil
+}
+
+func (a *App) jobToDeployJobRequest(job *config.Job) *pb.DeployJobRequest {
+	count := int32(job.Count)
+	container := job.Container[0]
+
+	ports := []*pb.Port{}
+
+	for _, port := range container.Ports {
+		ports = append(ports, &pb.Port{In: port.In})
+	}
+
+	return &pb.DeployJobRequest{
+		Count: count,
+		Container: &pb.Container{
+			Name:  container.Name,
+			Image: container.Image,
+			Ports: ports,
+		},
+	}
 }
 
 func (a *App) showNodeStatuses(inventory *config.Inventory, job *config.Job) error {
