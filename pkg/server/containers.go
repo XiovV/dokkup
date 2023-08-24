6b8bc5122d658b1bca7d30c8b561d28769a72e06
XiovV <xiovv@tutanota.com>
2023-08-24 17:38:50 +0200

    removed temporary code for detecting if an update should be run

--- pkg/server/containers.go
+++ pkg/server/containers.go
@@ -9,24 +9,10 @@ import (
 
 func (s *Server) DeployJob(request *pb.DeployJobRequest, stream pb.Dokkup_DeployJobServer) error {
 	s.Logger.Info("job received", zap.String("jobName", request.Name), zap.Int("containerCount", int(request.Count)), zap.String("containerImage", request.Container.Image))
-
-	doesJobExist, err := s.Controller.ContainerDoesExist(request.Name)
-	if err != nil {
-		s.Logger.Error("failed to check if deployment already exists", zap.Error(err))
-		return err
-	}
-
-	if doesJobExist {
-		s.Logger.Info("job already exists, run an update")
-		return nil
-	}
-
-	s.Logger.Info("job doesn't exist, run a full deployment")
-
 	stream.Send(&pb.DeployJobResponse{Message: fmt.Sprintf("Attempting to pull image: %s", request.Container.Image)})
 
 	s.Logger.Info("attempting to pull image", zap.String("image", request.Container.Image))
-	err = s.Controller.ImagePull(request.Container.Image)
+	err := s.Controller.ImagePull(request.Container.Image)
 	if err != nil {
 		s.Logger.Error("failed to pull image", zap.Error(err), zap.String("image", request.Container.Image))
 		return fmt.Errorf("failed to pull image: %w", err)
