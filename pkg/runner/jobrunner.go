af06ca1e3b8f842603759005a6d36fa9bcef9e90
XiovV <xiovv@tutanota.com>
2023-09-08 23:13:13 +0200

    removed unneccessary method calls

--- pkg/runner/jobrunner.go
+++ pkg/runner/jobrunner.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/XiovV/dokkup/pkg/docker"
 	pb "github.com/XiovV/dokkup/pkg/grpc"
+	"github.com/docker/docker/api/types"
 )
 
 type JobRunner struct {
@@ -15,8 +16,8 @@ func NewJobRunner(controller *docker.Controller) *JobRunner {
 	return &JobRunner{Controller: controller}
 }
 
-func (j *JobRunner) ShouldUpdateJob(request *pb.DeployJobRequest) (bool, error) {
-	runningContainers, err := j.Controller.GetContainersByJobName(request.Name)
+func (j *JobRunner) ShouldUpdateJob(jobName string, comparisonContainer types.ContainerJSON) (bool, error) {
+	runningContainers, err := j.Controller.GetContainersByJobName(jobName)
 	if err != nil {
 		return false, err
 	}
@@ -30,7 +31,7 @@ func (j *JobRunner) ShouldUpdateJob(request *pb.DeployJobRequest) (bool, error) 
 		return false, err
 	}
 
-	isDifferent := j.Controller.IsConfigDifferent(containerConfig, request)
+	isDifferent := j.Controller.IsConfigDifferent(containerConfig, comparisonContainer)
 	if !isDifferent {
 		return false, nil
 	}
@@ -58,21 +59,11 @@ func (j *JobRunner) RunDeployment(stream pb.Dokkup_DeployJobServer, request *pb.
 		return err
 	}
 
-	err = j.Controller.StartContainers(createdContainers, stream)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return j.Controller.StartContainers(createdContainers, stream)
 }
 
-func (j *JobRunner) RunUpdate(stream pb.Dokkup_DeployJobServer, request *pb.DeployJobRequest) error {
-	err := j.Controller.ImagePull(request.Container.Image)
-	if err != nil {
-		return fmt.Errorf("failed to pull image: %w", err)
-	}
-
-	oldContainers, err := j.Controller.GetContainersByJobName(request.Name)
+func (j *JobRunner) RunUpdate(jobName string, newContainers []string, stream pb.Dokkup_DeployJobServer) error {
+	oldContainers, err := j.Controller.GetContainersByJobName(jobName)
 	if err != nil {
 		return err
 	}
@@ -82,20 +73,13 @@ func (j *JobRunner) RunUpdate(stream pb.Dokkup_DeployJobServer, request *pb.Depl
 		return err
 	}
 
-	updatedContainers, err := j.Controller.CreateContainersFromRequest(request, stream)
-	if err != nil {
-		return err
-	}
-
 	err = j.Controller.StopContainers(oldContainers)
 	if err != nil {
 		return err
 	}
 
-	err = j.Controller.StartContainers(updatedContainers, stream)
-	if err != nil {
-		return err
-	}
+	return j.Controller.StartContainers(newContainers, stream)
+}
 
-	return nil
+func (j *JobRunner) AbortUpdate() {
 }
