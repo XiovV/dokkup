70028cf19b6b40390355025b8fba47c92c2c5320
XiovV <xiovv@tutanota.com>
2023-09-15 21:35:40 +0200

    started implementing the rollback command

--- pkg/cli/rollback.go
+++ pkg/cli/rollback.go
@@ -1,7 +1,59 @@
 package cli
 
-import "github.com/urfave/cli/v2"
+import (
+	"fmt"
+	"log"
+
+	"github.com/XiovV/dokkup/pkg/config"
+	"github.com/urfave/cli/v2"
+)
 
 func (a *App) rollbackCmd(ctx *cli.Context) error {
+	job, inventory, err := a.readJobAndInventory(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.showJobSummaryTable(job)
+
+	err = a.showNodeStatuses(inventory, job)
+	if err != nil {
+		log.Fatal("couldn't show node statuses: ", err)
+	}
+
+	shouldContinue, err := a.showConfirmationPrompt(ctx)
+	if err != nil {
+		log.Fatal("confirmation prompt error: ", err)
+	}
+
+	if !shouldContinue {
+		return nil
+	}
+
+	fmt.Print("\n")
+
 	return nil
 }
+
+func (a *App) rollbackJobs(job *config.Job, inventory *config.Inventory) error {
+	group, ok := inventory.GetGroup(job.Group)
+	if !ok {
+		return fmt.Errorf("couldn't find group: %s", group.Name)
+	}
+
+	for _, nodeName := range group.Nodes {
+		node, ok := inventory.GetNode(nodeName)
+		if !ok {
+			return fmt.Errorf("couldn't find node: %s", nodeName)
+		}
+
+		err := a.deployJob(node, job)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *App) rollbackJob(node config.Node, job *config.Job)
