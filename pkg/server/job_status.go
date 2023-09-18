5f590034aae78d8963bb1f3d1e4e307b1c16a4c8
XiovV <xiovv@tutanota.com>
2023-09-18 16:57:49 +0200

    added debug logs and logic for determining if an update should be run and returning the reponse back to the client

--- pkg/server/job_status.go
+++ pkg/server/job_status.go
@@ -9,23 +9,76 @@ import (
 )
 
 func (s *Server) GetJobStatus(ctx context.Context, in *pb.Job) (*pb.JobStatus, error) {
+	s.Logger.Info("retreiving job status")
+
+	s.Logger.Debug("fetching running containers")
 	runningContainers, err := s.Controller.GetContainers(in.Name, docker.GetContainersOptions{Stopped: false})
 	if err != nil {
 		s.Logger.Error("could not get running containers", zap.Error(err))
 		return nil, err
 	}
 
+	s.Logger.Debug("fetching all containers")
 	totalContainers, err := s.Controller.GetContainers(in.Name, docker.GetContainersOptions{Stopped: true})
 	if err != nil {
 		s.Logger.Error("could not get all containers", zap.Error(err))
 		return nil, err
 	}
 
+	s.Logger.Debug("checking if an image already exists", zap.String("image", in.Container.Image))
+	doesExist, err := s.Controller.ImageDoesExist(in.Container.Image)
+	if err != nil {
+		s.Logger.Error("failed to check if image exists", zap.Error(err), zap.String("image", in.Container.Image))
+		return nil, err
+	}
+
+	s.Logger.Debug("does image exist", zap.Bool("doesExist", doesExist))
+	shouldUpdate := !doesExist
+
 	response := &pb.JobStatus{
 		TotalContainers:   int32(len(totalContainers)),
 		RunningContainers: int32(len(runningContainers)),
-		ShouldUpdate:      false,
+		ShouldUpdate:      shouldUpdate,
 	}
+
+	s.Logger.Debug("should update job", zap.Bool("shouldUpdate", shouldUpdate))
+
+	if len(totalContainers) == 0 {
+		return response, nil
+	}
+
+	containerConfig, err := s.Controller.ContainerInspect(totalContainers[0].ID)
+	if err != nil {
+		return nil, err
+	}
+
+	s.Logger.Debug("creating temporary container")
+	temporaryContainer, err := s.Controller.CreateTemporaryContainer(in)
+	if err != nil {
+		s.Logger.Error("failed to create temporary container", zap.Error(err))
+		return nil, err
+	}
+
+	s.Logger.Debug("temporary container created successfully", zap.String("containerId", temporaryContainer))
+	temporaryContainerConfig, err := s.Controller.ContainerInspect(temporaryContainer)
+	if err != nil {
+		s.Logger.Error("could not inspect temporary container", zap.Error(err))
+		return nil, err
+	}
+
+	s.Logger.Debug("checking if configs are different")
+	isDifferent := s.Controller.IsConfigDifferent(containerConfig, temporaryContainerConfig)
+
+	s.Logger.Debug("is config different", zap.Bool("isDifferent", isDifferent))
+
+	err = s.Controller.ContainerRemove(temporaryContainer)
+	if err != nil {
+		s.Logger.Error("failed to remove temporary container", zap.Error(err))
+		return nil, err
+	}
+
+	response.ShouldUpdate = isDifferent
+	s.Logger.Debug("should update job", zap.Bool("shouldUpdate", isDifferent))
 
 	return response, nil
 }
