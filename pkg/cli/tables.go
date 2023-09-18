1fc4f72d74d5a664d18fbddec2ed913a681bd5de
XiovV <xiovv@tutanota.com>
2023-09-18 17:45:40 +0200

    added method for showing node statuses for the stop job command

--- pkg/cli/tables.go
+++ pkg/cli/tables.go
@@ -54,6 +54,18 @@ func (a *App) showDeployJobStatuses(jobStatuses []JobStatus) error {
 	return nil
 }
 
+func (a *App) showJobSummaryTable(job *config.Job) {
+	fmt.Print("Deployment summary:\n\n")
+
+	jobSummaryTable := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', 0)
+	fmt.Fprintln(jobSummaryTable, "NAME\tIMAGE\tRESTART\tCOUNT\tGROUP\tNETWORK")
+
+	out := fmt.Sprintf("%s\t%s\t%s\t%d\t%s\t%s\n", job.Name, job.Container[0].Image, job.Container[0].Restart, job.Count, job.Group, job.Container[0].Network)
+	fmt.Fprintln(jobSummaryTable, out)
+
+	jobSummaryTable.Flush()
+}
+
 func (a *App) showStopJobSummaryTable(job *config.Job) {
 	fmt.Print("Stop job summary:\n\n")
 
@@ -66,14 +78,34 @@ func (a *App) showStopJobSummaryTable(job *config.Job) {
 	jobSummaryTable.Flush()
 }
 
-func (a *App) showJobSummaryTable(job *config.Job) {
-	fmt.Print("Deployment summary:\n\n")
+func (a *App) showStopJobStatuses(jobStatuses []JobStatus) error {
+	fmt.Print("Node statuses:\n\n")
+	nodeStatusesTable := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', 0)
+	fmt.Fprintln(nodeStatusesTable, "NAME\tSTATUS\tCONTAINERS")
 
-	jobSummaryTable := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', 0)
-	fmt.Fprintln(jobSummaryTable, "NAME\tIMAGE\tRESTART\tCOUNT\tGROUP\tNETWORK")
+	var unavailableNodes int
+	for _, jobStatus := range jobStatuses {
+		if jobStatus.NodeStatus == NODE_STATUS_OFFLINE || jobStatus.NodeStatus == NODE_STATUS_UNAUTHENTICATED {
+			out := fmt.Sprintf("%s\t%s\t%d/%d", jobStatus.Node.Name, jobStatus.NodeStatus, 0, 0)
+			fmt.Fprintln(nodeStatusesTable, out)
 
-	out := fmt.Sprintf("%s\t%s\t%s\t%d\t%s\t%s\n", job.Name, job.Container[0].Image, job.Container[0].Restart, job.Count, job.Group, job.Container[0].Network)
-	fmt.Fprintln(jobSummaryTable, out)
+			unavailableNodes++
+			continue
+		}
 
-	jobSummaryTable.Flush()
+		out := fmt.Sprintf("%s\t%s\t%d/%d", jobStatus.Node.Name, NODE_STATUS_ONLINE, jobStatus.RunningContainers, jobStatus.TotalContainers)
+		fmt.Fprintln(nodeStatusesTable, out)
+	}
+
+	nodeStatusesTable.Flush()
+
+	if unavailableNodes == 1 {
+		fmt.Printf("\nWarning! It seems that there is %d unavailable node, it will be skipped.\n", unavailableNodes)
+	}
+
+	if unavailableNodes > 1 {
+		fmt.Printf("\nWarning! It seems that there are %d unavailable nodes, they will be skipped.\n", unavailableNodes)
+	}
+
+	return nil
 }
