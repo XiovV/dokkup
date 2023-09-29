d0cce6774331901f06a1b77f11f60bafb196bcc4
XiovV <xiovv@tutanota.com>
2023-09-29 09:57:18 +0200

    refactor(agent): clean up DeployJob RPC

--- pkg/server/deploy.go
+++ pkg/server/deploy.go
@@ -12,54 +12,52 @@ func (s *Server) DeployJob(request *pb.Job, stream pb.Dokkup_DeployJobServer) er
 	doesJobExist := s.JobRunner.DoesJobExist(request.Name)
 
 	s.Logger.Debug("does job already exist", zap.Bool("doesJobExist", doesJobExist))
-
-	if doesJobExist {
-
-		s.Logger.Debug("checking if the job should be updated")
-		shouldUpdate, err := s.JobRunner.ShouldUpdateJob(request)
+	if !doesJobExist {
+		s.Logger.Info("deploying a new job")
+		err := s.JobRunner.RunDeployment(stream, request)
 		if err != nil {
-			s.Logger.Error("failed to check if an update should be run", zap.Error(err))
+			s.Logger.Error("could not deploy job", zap.Error(err))
 			return err
 		}
 
-		s.Logger.Debug("should update job", zap.Bool("shouldUpdate", shouldUpdate))
+		s.Logger.Info("job completed successfully")
+		stream.Send(&pb.DeployJobResponse{Message: "Deployment successful"})
+		return nil
+	}
 
-		if !shouldUpdate {
-			stoppedContainers, err := s.Controller.GetStoppedContainers(request.Name)
-			if err != nil {
-				s.Logger.Error("could not get stopped containers", zap.Error(err))
-				return err
-			}
+	s.Logger.Debug("checking if the job should be updated")
+	shouldUpdate, err := s.JobRunner.ShouldUpdateJob(request)
+	if err != nil {
+		s.Logger.Error("failed to check if an update should be run", zap.Error(err))
+		return err
+	}
 
-			if len(stoppedContainers) == 0 {
-				s.Logger.Debug("nothing to do, exiting...")
-				stream.Send(&pb.DeployJobResponse{Message: "Already up to date"})
-				return nil
-			}
+	s.Logger.Debug("should update job", zap.Bool("shouldUpdate", shouldUpdate))
 
-			for _, container := range stoppedContainers {
-				err := s.Controller.ContainerStart(container.ID)
-				if err != nil {
-					s.Logger.Error("could not start container", zap.Error(err), zap.String("containerId", container.ID))
-					return err
-				}
-			}
-
-			return nil
-		}
-
+	if shouldUpdate {
 		s.Logger.Info("updating job", zap.String("jobName", request.Name))
 		return s.JobRunner.RunUpdate(request, stream)
 	}
 
-	s.Logger.Info("deploying a new job")
-	err := s.JobRunner.RunDeployment(stream, request)
+	stoppedContainers, err := s.Controller.GetStoppedContainers(request.Name)
 	if err != nil {
-		s.Logger.Error("could not deploy job", zap.Error(err))
+		s.Logger.Error("could not get stopped containers", zap.Error(err))
 		return err
 	}
 
-	s.Logger.Info("job completed successfully")
-	stream.Send(&pb.DeployJobResponse{Message: "Deployment successful"})
+	if len(stoppedContainers) == 0 {
+		s.Logger.Debug("nothing to do, exiting...")
+		stream.Send(&pb.DeployJobResponse{Message: "Already up to date"})
+		return nil
+	}
+
+	err = s.JobRunner.StartContainers(stoppedContainers, stream)
+	if err != nil {
+		s.Logger.Error("could not start container", zap.Error(err))
+		return err
+	}
+
+	stream.Send(&pb.DeployJobResponse{Message: "Containers started successfully"})
+
 	return nil
 }
